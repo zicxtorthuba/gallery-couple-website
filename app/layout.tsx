05a2ed6eeac981c16da3b0ac8212270c8738720e
zicxtorthuhai <218626138+zicxtorthuba@users.noreply.github.com>
2025-06-30 23:50:03 +0700

    update upload function

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter, Cormorant_Garamond } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { EdgeStoreProvider } from '@/lib/edgestore';
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -36,8 +37,10 @@ export default function RootLayout({
           defaultTheme="light"
           enableSystem={false}
         >
-          {children}
-          <Toaster />
+          <EdgeStoreProvider>
+            {children}
+            <Toaster />
+          </EdgeStoreProvider>
         </ThemeProvider>
       </body>
     </html>
